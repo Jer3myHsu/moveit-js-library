7656fbc36a0329e2b5c98ba3e0aa3b48b93a37f7
Jer3myHsu <34175005+Jer3myHsu@users.noreply.github.com>
2020-08-21 03:18:13 -0400

    Added notFound.js

---
+++ pub/js/notFound.js
@@ -0,0 +1,8 @@
+"user strict";
+
+MoveIt.initializeMoveIt("notFound");
+
+MoveIt.elementOriginStyle = "visibility: hidden;";
+MoveIt.elementHeldStyle = "font-size: 2em; font-weight: bold;"
+MoveIt.elementHoverStyle = "color: #225CAD";
+MoveIt.holdCursor = "grabbing";
